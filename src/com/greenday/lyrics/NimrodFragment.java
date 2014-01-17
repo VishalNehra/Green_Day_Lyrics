0ea195b01e2736c03af116763b67a22886e0de45
Vishal Nehra <vishalmeham2@gmail.com>
2014-01-17 23:13:34 +0530

    Added more lyrics
    Added lyrics of some more songs

--- src/com/greenday/lyrics/NimrodFragment.java
+++ src/com/greenday/lyrics/NimrodFragment.java
@@ -1,10 +1,31 @@
 package com.greenday.lyrics;
 
+import com.greenday.americanidiot.Americanidiot;
+import com.greenday.americanidiot.Arewethewaiting;
+import com.greenday.americanidiot.Boulevardofbd;
+import com.greenday.americanidiot.Extraordgirl;
+import com.greenday.americanidiot.Givemenov;
+import com.greenday.americanidiot.Holiday;
+import com.greenday.americanidiot.Homecoming;
+import com.greenday.americanidiot.Jesusofsuburb;
+import com.greenday.americanidiot.Letterbomb;
+import com.greenday.americanidiot.Shesarebel;
+import com.greenday.americanidiot.Stjimmy;
+import com.greenday.americanidiot.Wakemeup;
+import com.greenday.americanidiot.Whatshername;
+import com.greenday.nimrod.Allthetime;
+import com.greenday.nimrod.Goodriddance;
+import com.greenday.nimrod.Haushinka;
+import com.greenday.nimrod.Hitchinaride;
+import com.greenday.nimrod.Niceguys;
+
 import android.os.Bundle;
 import android.app.Fragment;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -21,34 +42,103 @@ public class NimrodFragment extends Fragment {
         ListView listview = (ListView) rootView.findViewById(R.id.listView1);
 
         //EDITED Code 
-        String[] items = new String[] {"\t\t\t\t\tAll The Time",
-        		"\t\t\t\t\tGood Riddance (Time Of Your Life)",
-        		"\t\t\t\t\tHaushinka",
-        		"\t\t\t\t\tHitchin' a Ride",
-        		"\t\t\t\t\tJinx",
-        		"\t\t\t\t\tKing For A Day",
-        		"\t\t\t\t\tLast Ride In",
-        		"\t\t\t\t\tNice Guys Finish Last",
-        		"\t\t\t\t\tPlatypus (I Hate You)",
-        		"\t\t\t\t\tProsthetic Head",
-        		"\t\t\t\t\tRedundant",
-        		"\t\t\t\t\tReject",
-        		"\t\t\t\t\tScattered",
-        		"\t\t\t\t\tTake Back",
+        String[] values = new String[] {"\t\t\t\t\tNice Guys Finish Last",
+        		"\t\t\t\t\tHitchin' A Ride",
         		"\t\t\t\t\tThe Grouch",
+        		"\t\t\t\t\tRedundant",
+        		"\t\t\t\t\tScattered",
+        		"\t\t\t\t\tAll The Time",
+        		"\t\t\t\t\tWorry Rock",
+        		"\t\t\t\t\tPlatypus (I Hate You)",
         		"\t\t\t\t\tUptight",
+        		"\t\t\t\t\tLast Ride In",
+        		"\t\t\t\t\tJinx",
+        		"\t\t\t\t\tHaushinka",
         		"\t\t\t\t\tWalking Alone",
-        		"\t\t\t\t\tWorry Rock"};
+        		"\t\t\t\t\tReject",
+        		"\t\t\t\t\tTake Back",
+        		"\t\t\t\t\tKing For A Day",
+        		"\t\t\t\t\tGood Riddance (Time Of Your Life)",
+        		"\t\t\t\t\tProsthetic Head"};
         ArrayAdapter<String> adapter =
-        new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items); 
+                new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, values); 
+                super.onActivityCreated(savedInstanceState);
+                 listview.setAdapter(adapter);  
 
-         listview.setAdapter(adapter);  
-
-         //To have custom list view use this : you must define CustomeAdapter class
-         // listview.setadapter(new CustomeAdapter(getActivity()));
-        //getActivty is used instead of Context
-        
-        return rootView;
-    }
-
-}
+                 //To have custom list view use this : you must define CustomeAdapter class
+                 // listview.setadapter(new CustomeAdapter(getActivity()));
+                //getActivty is used instead of Context
+                /*
+                 * Adding individual onclicklistener commands below;  
+                 */
+                 
+                 listview.setOnItemClickListener(new AdapterView.OnItemClickListener() 
+                 { 
+                	 public void onItemClick(AdapterView<?> p, View view, int position, long id)
+                	 
+                	 { 
+                		 Fragment fragment = null;
+                		 switch(position)
+                         {
+                         case 0:  Intent newActivity = new Intent(getActivity(), Niceguys.class);     
+                         		  startActivityForResult(newActivity, 0);
+                                  break;
+                         case 1:  Intent newActivity1 = new Intent(getActivity(), Hitchinaride.class);     
+                                  startActivityForResult(newActivity1, 1);
+                                  break;
+                         case 2:  Intent newActivity2 = new Intent(getActivity(), Haushinka.class);     
+        			              startActivityForResult(newActivity2, 1);
+        			              break;
+                         case 3:  Intent newActivity3 = new Intent(getActivity(), Hitchinaride.class);     
+        	              		  startActivityForResult(newActivity3, 1);
+        	              		  break;
+                         case 4:  Intent newActivity4 = new Intent(getActivity(), Arewethewaiting.class);     
+        		         		  startActivityForResult(newActivity4, 1);
+        		         		  break;
+                         case 5:  Intent newActivity5 = new Intent(getActivity(), Allthetime.class);     
+        		        		  startActivityForResult(newActivity5, 1);
+        		        		  break;
+                         case 6:  Intent newActivity6 = new Intent(getActivity(), Givemenov.class);     
+        			       		  startActivityForResult(newActivity6, 1);
+        			       		  break;
+                         case 7:  Intent newActivity7 = new Intent(getActivity(), Shesarebel.class);     
+        			       		  startActivityForResult(newActivity7, 1);
+        			       		  break;
+                         case 8:  Intent newActivity8 = new Intent(getActivity(), Extraordgirl.class);     
+        			       		  startActivityForResult(newActivity8, 1);
+        			       		  break;
+                         case 9:  Intent newActivity9 = new Intent(getActivity(), Letterbomb.class);     
+        			       		  startActivityForResult(newActivity9, 1);
+        			       		  break; 
+                         case 10: Intent newActivity10 = new Intent(getActivity(), Wakemeup.class);     
+        			       		  startActivityForResult(newActivity10, 1);
+        			       		  break; 
+                         case 11: Intent newActivity11 = new Intent(getActivity(), Haushinka.class);     
+        			       		  startActivityForResult(newActivity11, 1);
+        			       		  break;
+                         case 12: Intent newActivity12 = new Intent(getActivity(), Whatshername.class);     
+        			       		  startActivityForResult(newActivity12, 1);
+        			       		  break;
+                         case 13: Intent newActivity13 = new Intent(getActivity(), Whatshername.class);     
+					       		  startActivityForResult(newActivity13, 1);
+					       		  break;
+                         case 14: Intent newActivity14 = new Intent(getActivity(), Whatshername.class);     
+					       		  startActivityForResult(newActivity14, 1);
+					       		  break; 
+                         case 15: Intent newActivity15 = new Intent(getActivity(), Whatshername.class);     
+					       		  startActivityForResult(newActivity15, 1);
+					       		  break; 
+                         case 16: Intent newActivity16 = new Intent(getActivity(), Goodriddance.class);     
+					       		  startActivityForResult(newActivity16, 1);
+					       		  break;  		  
+                         case 17: Intent newActivity17 = new Intent(getActivity(), Whatshername.class);     
+					       		  startActivityForResult(newActivity17, 1);
+					       		  break;  		  
+                  
+                      }
+                	 }
+                });
+                return rootView;
+            }
+           
+        }
